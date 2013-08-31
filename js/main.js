1567f189b28f169f4b28ed88dc22c1e42f884fe3
danbjoseph <danbjoseph@gmail.com>
2013-08-31 17:26:25 -0400

    spinning globe, starting on page nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
-$('#myCarousel').carousel({
-    pause: "false",
-    interval: 3000
+function toggleHidden(target){
+    $('.hidden-box').hide();
+    $(target).show();
+}
+
+
+$(document).ready(function() {
+	$('#myCarousel').carousel({
+		interval: 6000,
+		pause: "false"
+	});
 });
+
+
+var width = 500,
+    height = 500;
+
+var lat = -180
+var projection = d3.geo.orthographic()
+    .scale(200)
+    .translate([width/2,height/2])
+    .rotate([lat,0])
+    .clipAngle(90);
+var path = d3.geo.path()
+    .projection(projection);
+
+var graticule = d3.geo.graticule();
+
+var svg = d3.select("#rotatingGlobe").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("../data/world.json", function(collection){
+	svg.selectAll("path")
+		.data(collection.features)
+		.enter().append("path")
+        .attr("d", path)
+        .attr("class", function(d){return d.properties.featurecla;}); 
+});
+
+
+setInterval(function(){
+lat = lat +.5
+ projection.rotate([lat,0]);
+  svg.selectAll("path")
+      .attr("d", path);
+},window.anispeed);
+
+
