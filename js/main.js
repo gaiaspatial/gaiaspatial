c3a4d4b430bd929dfc33d91d11a643894cb9e373
danbjoseph <danbjoseph@gmail.com>
2013-10-06 19:51:28 -0400

    working on new page structure

--- js/main.js
+++ js/main.js
@@ -22,89 +22,79 @@ function changeLandscapeLayer(newLayer) {
 }
 
 
-// function toggleContentOption(target){
-//     $('.content-option').hide();
-//     $(target).show();
-// }
+var lat = -180
+var projection = d3.geo.orthographic()
+    .rotate([lat,0])
+    .clipAngle(90);
+var path = d3.geo.path()
+    .projection(projection);
+var rscale = d3.scale.sqrt();
+var graticule = d3.geo.graticule();
+var world;
+var width = 0;
+var height = 0;
 
-// $(document).ready(function() {
-// 	$('#myCarousel').carousel({
-// 		interval: 8000,
-// 		pause: "false"
-// 	});
-// });
+var svg = d3.select("#globe").append("svg")
+    .attr("width", 1)
+    .attr("height", 1);
 
-// var lat = -180
-// var projection = d3.geo.orthographic()
-//     .rotate([lat,0])
-//     .clipAngle(90);
-// var path = d3.geo.path()
-//     .projection(projection);
-// var rscale = d3.scale.sqrt();
-// var graticule = d3.geo.graticule();
-// var world;
+function getGlobeData(){
+  $.ajax({
+      type: 'GET',
+      url: 'data/world.json',
+      contentType: 'application/json',
+      dataType: 'json',
+      timeout: 10000,
+      success: function(json) {
+        world = json;        
+        initSizes();
+      },
+      error: function(e) {
+          console.log(e);
+      }
+  });
+}
 
-// var svg = d3.select(".rotatingGlobe").append("svg")
-//     .attr("width", 1)
-//     .attr("height", 1);
-
-// function getGlobeData(){
-//   $.ajax({
-//       type: 'GET',
-//       url: 'data/world.json',
-//       contentType: 'application/json',
-//       dataType: 'json',
-//       timeout: 10000,
-//       success: function(json) {
-//         world = json;        
-//         initSizes();
-//       },
-//       error: function(e) {
-//           console.log(e);
-//       }
-//   });
-// }
-
-// function initSizes() {
-//   height = $(".logo").height() + 40;
-//   width = height;
-
-//   projection.translate([width/2,height/2]);
-//   svg
-//     .attr("width", width)
-//     .attr("height", height);
-//   // rscale.range([0, height]);
-//   fitMapProjection();
-// };
+function initSizes() {
+  width = $("#globe").width();
+  if(width > 150){
+    width = 150;
+  };
+  height = width;
+  projection.translate([width/2,height/2]);
+  svg
+    .attr("width", width)
+    .attr("height", height);
+  // rscale.range([0, height]);
+  fitMapProjection();
+};
 
 
 
-// var fitMapProjection = function() {
-//   fitProjection(projection, world, [[0, 0], [width-10, height-10]], true);
-//   drawGlobe();
-// };
+var fitMapProjection = function() {
+  fitProjection(projection, world, [[10, 10], [width-15, height-15]], true);
+  drawGlobe();
+};
 
 
 
-// function drawGlobe(){ 
-//     fitMapProjection();
-//     svg.selectAll("path")
-//         .data(world.features)
-//         .enter().append("path")
-//         .attr("d", path)
-//         .attr("class", function(d){return d.properties.featurecla;}); 
-//   setInterval(function(){
-//   lat = lat +.5
-//    projection.rotate([lat,0]);
-//     svg.selectAll("path")
-//         .attr("d", path);
-//   },window.anispeed);      
-// }
+function drawGlobe(){ 
+    svg.selectAll("path")
+        .data(world.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("class", function(d){return d.properties.featurecla;}); 
+  setInterval(function(){
+    lat = lat + .5;
+    projection.rotate([lat,0]);
+    svg.selectAll("path").attr("d", path);
+  },window.anispeed);      
+}
 
 
 
-// getGlobeData();
+getGlobeData();
 
-// d3.select(window).on('resize', function resize() {
-//     initSizes();    
-// });
+d3.select(window).on('resize', function resize() {
+    initSizes();    
+});
